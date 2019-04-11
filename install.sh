f167b9e104b4582c8eca5cf25d3a685d9e557c04
tianhao <tianhaolsk@qq.com>
2019-04-11 22:42:38 +0800

    set network first

--- install.sh
+++ install.sh
@@ -1,7 +1,42 @@
 #!/usr/bin/env bash
+
+NODE_NUM=$1
+NODE_IP=$2
+ETCD_CLUSTER=$3
+echo "========================================"
+echo "NODE_NUM=${NODE_NUM}"
+echo "NODE_IP=${NODE_IP}"
+echo "ETCD_CLUSTER=${ETCD_CLUSTER}"
+echo "========================================"
+
 # change time zone
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 timedatectl set-timezone Asia/Shanghai
+
+echo '======== enable iptable kernel parameter ========'
+cat >> /etc/sysctl.conf <<EOF
+net.ipv4.ip_forward=1
+EOF
+sysctl -p
+
+echo '======== set host name resolution ========'
+cat >> /etc/hosts <<EOF
+172.17.8.101 node1
+172.17.8.102 node2
+172.17.8.103 node3
+EOF
+cat /etc/hosts
+
+echo '======== set nameserver ========'
+echo "nameserver 8.8.4.4" >>/etc/resolv.conf
+echo "nameserver 8.8.8.8" >>/etc/resolv.conf
+cat /etc/resolv.conf
+
+echo '======== disable swap ========'
+swapoff -a
+sed -i '/swap/s/^/#/' /etc/fstab
+
+echo "set repo"
 rm /etc/yum.repos.d/CentOS-Base.repo
 cp /vagrant/yum/*.* /etc/yum.repos.d/
 mv /etc/yum.repos.d/CentOS7-Base-163.repo /etc/yum.repos.d/CentOS-Base.repo
@@ -15,36 +50,14 @@ if [[ $(hostname) == "node1" ]] && [[ ! -f "$kubernetes_release" ]]; then
 fi
 
 # enable ntp to sync time
-echo 'sync time'
+echo '======== sync time ========'
 systemctl start ntpd
 systemctl enable ntpd
-echo 'disable selinux'
+echo '======== disable selinux ========'
 setenforce 0
 sed -i 's/=enforcing/=disabled/g' /etc/selinux/config
 
-echo 'enable iptable kernel parameter'
-cat >> /etc/sysctl.conf <<EOF
-net.ipv4.ip_forward=1
-EOF
-sysctl -p
-
-echo 'set host name resolution'
-cat >> /etc/hosts <<EOF
-172.17.8.101 node1
-172.17.8.102 node2
-172.17.8.103 node3
-EOF
-
-cat /etc/hosts
-
-echo 'set nameserver'
-echo "nameserver 8.8.8.8">/etc/resolv.conf
-cat /etc/resolv.conf
-
-echo 'disable swap'
-swapoff -a
-sed -i '/swap/s/^/#/' /etc/fstab
-
+echo '======== install and set docker ========'
 #create group if not exists
 egrep "^docker" /etc/group >& /dev/null
 if [ $? -ne 0 ]
@@ -64,26 +77,28 @@ cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 
-if [[ $1 -eq 1 ]]
+echo '======== install and set etcd ========'
+if [[ $NODE_NUM -eq 1 ]]
 then
     yum install -y etcd
     #cp /vagrant/systemd/etcd.service /usr/lib/systemd/system/
 cat > /etc/etcd/etcd.conf <<EOF
 #[Member]
 ETCD_DATA_DIR="/var/lib/etcd/default.etcd"
-ETCD_LISTEN_PEER_URLS="http://$2:2380"
-ETCD_LISTEN_CLIENT_URLS="http://$2:2379,http://localhost:2379"
-ETCD_NAME="node$1"
+ETCD_LISTEN_PEER_URLS="http://$NODE_IP:2380"
+ETCD_LISTEN_CLIENT_URLS="http://$NODE_IP:2379,http://localhost:2379"
+ETCD_NAME="node$NODE_NUM"
 
 #[Clustering]
-ETCD_INITIAL_ADVERTISE_PEER_URLS="http://$2:2380"
-ETCD_ADVERTISE_CLIENT_URLS="http://$2:2379"
-ETCD_INITIAL_CLUSTER="$3"
+ETCD_INITIAL_ADVERTISE_PEER_URLS="http://$NODE_IP:2380"
+ETCD_ADVERTISE_CLIENT_URLS="http://$NODE_IP:2379"
+ETCD_INITIAL_CLUSTER="$ETCD_CLUSTER"
 ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster"
 ETCD_INITIAL_CLUSTER_STATE="new"
 EOF
-    cat /etc/etcd/etcd.conf
-    echo 'create network config in etcd'
+
+cat /etc/etcd/etcd.conf
+echo 'create network config in etcd'
 cat > /etc/etcd/etcd-init.sh<<EOF
 #!/bin/bash
 etcdctl mkdir /kube-centos/network
@@ -101,7 +116,7 @@ EOF
     etcdctl ls /
 fi
 
-echo 'install flannel...'
+echo '======== install flannel... ========'
 yum install -y flannel
 
 echo 'create flannel config file...'
@@ -119,12 +134,12 @@ systemctl daemon-reload
 systemctl enable flanneld
 systemctl start flanneld
 
-echo 'enable docker'
+echo '======== enable docker ========'
 systemctl daemon-reload
 systemctl enable docker
 systemctl start docker
 
-echo "copy pem, token files"
+echo "======== copy pem, token files ========"
 mkdir -p /etc/kubernetes/ssl
 cp /vagrant/pki/* /etc/kubernetes/ssl/
 cp /vagrant/conf/token.csv /etc/kubernetes/
@@ -141,9 +156,9 @@ mkdir -p /var/lib/kubelet
 mkdir -p ~/.kube
 cp /vagrant/conf/admin.kubeconfig ~/.kube/config
 
-if [[ $1 -eq 1 ]]
+if [[ $NODE_NUM -eq 1 ]]
 then
-    echo "configure master and node1"
+    echo "======== configure master and node1 ========"
 
     cp /vagrant/conf/apiserver /etc/kubernetes/
     cp /vagrant/conf/config /etc/kubernetes/
@@ -169,9 +184,9 @@ then
     systemctl start kube-proxy
 fi
 
-if [[ $1 -eq 2 ]]
+if [[ $NODE_NUM -eq 2 ]]
 then
-    echo "configure node2"
+    echo "======== configure node2 ========"
     cp /vagrant/node2/* /etc/kubernetes/
 
     systemctl daemon-reload
@@ -181,9 +196,9 @@ then
     systemctl start kube-proxy
 fi
 
-if [[ $1 -eq 3 ]]
+if [[ $NODE_NUM -eq 3 ]]
 then
-    echo "configure node3"
+    echo "======== configure node3 ========"
     cp /vagrant/node3/* /etc/kubernetes/
 
     systemctl daemon-reload
@@ -210,7 +225,6 @@ then
     kubectl apply -f /vagrant/addon/traefik-ingress/
 fi
 
-echo "Configure Kubectl to autocomplete"
+echo "======== Configure Kubectl to autocomplete ========"
 source <(kubectl completion bash) # setup autocomplete in bash into the current shell, bash-completion package should be installed first.
 echo "source <(kubectl completion bash)" >> ~/.bashrc # add autocomplete permanently to your bash shell.
-
